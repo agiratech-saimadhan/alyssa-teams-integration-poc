ccddcdfb27698ee695a92db24bc3f77fc988cc54
sai madhan <saimadhan.k@agiratech.com>
2024-06-21 16:14:34 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CreateMeeting from "./components/createMeeting";
+
+function App() {
+  return (
+    <main className="grid place-items-center h-screen w-full">
+      <CreateMeeting />
+    </main>
+  );
+}
+
+export default App;
